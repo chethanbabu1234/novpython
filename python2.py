1feceafe4505968e17ee08d63e15aaf4c2317387
chethanbabu1234 <chethanbabu7345@gmail.com>
2024-12-24 09:31:15 +0530

    chethanclass

---
+++ python2.py
@@ -0,0 +1,118 @@
+# num=int(input('enter number:'))
+# if num<0:
+#     print('-ve number')
+# elif num>0:
+#     print('+ve number')
+# else:
+#     print('number is zero')
+
+
+#maths=int(input('enter maths marks'))
+#phy=int(input('enter phy marks'))
+#chem=int(input('enter chem marks'))
+#total_marks=600
+#sum_marks=maths+phy+chem
+#per=(sum_marks/total_marks)*100
+#print(per)
+#if per<35:
+#    print('grade is f')
+#elif per>35 and per<=50:
+#    print('grade is c')
+#elif per in range(51,91):
+#    print('grade is b')
+#elif per in range(91,101):
+#    print('grade is a')
+#else:
+#    print('invalid per')
+
+
+
+# def football_match_ticket():
+#     conversion_rate = 84
+#     age = int(input("Enter your age: "))
+#     if 18 <= age <= 30:
+#         cost_usd = 5
+#     elif 30 < age <= 50:
+#         cost_usd = 20
+#     elif age > 50:
+#         cost_usd = 0
+#     else:
+#         print("Sorry, tickets are not available for your age group.")
+#         return
+#     cost_inr = cost_usd * conversion_rate
+#     print(f"Your ticket cost is ${cost_usd}, which is ₹{cost_inr:.2f} in Indian Rupees.")
+# football_match_ticket()
+
+
+
+# name=input('enter name')
+# exp=int(input(f'enter{name}experience'))
+# if exp>=1:
+#     if exp>=1 and exp<=5:
+#         print('25k')
+#     elif exp>=6 and exp<=10:
+#         print('50k')
+#     else:
+#         print('75k')
+# else:
+#     print('not allowed')
+
+
+# while True:
+#  print('1.payment failed')
+#  print('2.profile and privacy')
+#  print('3.money transfer')
+#  print('4.exit')
+#  user_inp=int(input('enter your choice:'))
+#  if user_inp==1:
+#      print('welcome to payment failed')
+#  elif user_inp==2:
+#      print('welcome to profile and privacy')
+#  elif user_inp==3:
+#      print('welcome to money transfer')
+#  elif user_inp==4:
+#      print('exit')
+#      break
+ 
+
+
+# count=0
+# value="arjun"
+# for x in value:
+#     print(x)
+#     count=count+1
+#     print(count)
+
+
+
+
+# count=0
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+# print('total values of odd',count)
+
+
+# count=0
+# sum=0
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+#     sum=sum+x
+# print('total values of odd',count)
+# print('total sum of odd',sum)
+
+
+
+# count=0
+# sum=0
+# product=1
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+#     sum=sum+x
+#     product=product*x
+# print('total values of odd',count)
+# print('total sum of odd',sum)
+# print('total product of odd',product)
+                   
